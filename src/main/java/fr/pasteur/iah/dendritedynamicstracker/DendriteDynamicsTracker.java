0bf552b0c57ba4f7bbf62f5c066d3a02a759a438
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-06-17 13:49:58 +0200

    Initial source folders.

---
+++ src/main/java/fr/pasteur/iah/dendritedynamicstracker/DendriteDynamicsTracker.java
@@ -0,0 +1,14 @@
+package fr.pasteur.iah.dendritedynamicstracker;
+
+import ij.plugin.PlugIn;
+
+public class DendriteDynamicsTracker implements PlugIn
+{
+
+	@Override
+	public void run( final String arg )
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/test/java/fr/pasteur/iah/dendritedynamicstracker/TestDrive.java
@@ -0,0 +1,12 @@
+package fr.pasteur.iah.dendritedynamicstracker;
+
+public class TestDrive
+{
+
+	public static void main( final String[] args )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
