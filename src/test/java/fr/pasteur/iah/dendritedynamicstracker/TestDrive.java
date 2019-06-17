b69aca61be5420295e8eca1d94702c0c22da6d29
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-06-17 17:35:20 +0200

    Flesh out test drive.

--- src/test/java/fr/pasteur/iah/dendritedynamicstracker/TestDrive.java
+++ src/test/java/fr/pasteur/iah/dendritedynamicstracker/TestDrive.java
@@ -1,11 +1,23 @@
 package fr.pasteur.iah.dendritedynamicstracker;
 
+import java.io.IOException;
+
+import net.imagej.ImageJ;
+
 public class TestDrive
 {
 
-	public static void main( final String[] args )
+	public static void main( final String[] args ) throws IOException
 	{
-		// TODO Auto-generated method stub
+		final String traceImgName = "samples/traces2.tif";
+
+		final ImageJ ij = new ImageJ();
+		ij.launch( args );
+
+		final Object obj = ij.io().open( traceImgName );
+		ij.ui().show( obj );
+
+		new DendriteDynamicsTrackerPlugIn().run( null );
 
 	}
 
