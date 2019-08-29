1b62bf519b781128d0e587eb9e6ffc677104aa67
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-08-29 16:06:06 +0200

    Rewrite the SkeletonEndPointsDetector to use the skeleton graphs.
    - easier to define the position of a junction.
    - makes it possible to retrieve easily the junction id matching an
    end-point.

--- src/main/java/fr/pasteur/iah/dendritedynamicstracker/trackmate/SkeletonEndPointsDetector.java
+++ src/main/java/fr/pasteur/iah/dendritedynamicstracker/trackmate/SkeletonEndPointsDetector.java
@@ -1,15 +1,15 @@
 package fr.pasteur.iah.dendritedynamicstracker.trackmate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import org.apache.commons.math3.ml.clustering.Cluster;
-import org.apache.commons.math3.ml.clustering.DBSCANClusterer;
-import org.apache.commons.math3.ml.clustering.DoublePoint;
+import java.util.Map;
 
 import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.detection.DetectionUtils;
 import fiji.plugin.trackmate.detection.SpotDetector;
+import fr.pasteur.iah.dendritedynamicstracker.trackmate.feature.BranchLengthAnalyzerFactory;
+import fr.pasteur.iah.dendritedynamicstracker.trackmate.feature.JunctionIDAnalyzerFactory;
 import ij.ImagePlus;
 import ij.plugin.Duplicator;
 import net.imglib2.Interval;
@@ -20,8 +20,11 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 import sc.fiji.analyzeSkeleton.AnalyzeSkeleton_;
+import sc.fiji.analyzeSkeleton.Edge;
+import sc.fiji.analyzeSkeleton.Graph;
 import sc.fiji.analyzeSkeleton.Point;
 import sc.fiji.analyzeSkeleton.SkeletonResult;
+import sc.fiji.analyzeSkeleton.Vertex;
 
 public class SkeletonEndPointsDetector< T extends RealType< T > & NativeType< T > > implements SpotDetector< T >
 {
@@ -102,81 +105,92 @@ public class SkeletonEndPointsDetector< T extends RealType< T > & NativeType< T 
 		final long startx = interval.numDimensions() > 0 ? interval.min( 0 ) : 0;
 		final long starty = interval.numDimensions() > 1 ? interval.min( 1 ) : 0;
 		final long startz = interval.numDimensions() > 2 ? interval.min( 2 ) : 0;
+		final long[] topLeft = new long[] { startx, starty, startz };
 
-		final List< Point > endPoints = result.getListOfEndPoints();
-		for ( final Point point : endPoints )
+		final Graph[] graphs = result.getGraph();
+		for ( final Graph graph : graphs )
 		{
-			final double x = ( startx + point.x ) * calibration[ 0 ];
-			final double y = ( starty + point.y ) * calibration[ 1 ];
-			final double z = ( startz + point.z ) * calibration[ 2 ];
-			final double radius = 0.5;
-			final double quality = END_POINTS_QUALITY_VALUE;
-			final Spot spot = new Spot( x, y, z, radius, quality );
-			spots.add( spot );
-		}
+			final List< Vertex > vertices = graph.getVertices();
+			final Map< Vertex, Spot > vertexMap = new HashMap<>();
 
-		/*
-		 * Deal with junction voxels.
-		 */
+			/*
+			 * Find junctions.
+			 */
 
-		// Cluster junction pixels.
-		final List< DoublePoint > junctionPixels = new ArrayList<>( result.getListOfJunctionVoxels().size() );
-		for ( final Point point : result.getListOfJunctionVoxels() )
-		{
-			final double x = ( startx + point.x );
-			final double y = ( starty + point.y );
-			final double z = ( startz + point.z );
-			junctionPixels.add( new DoublePoint( new double[] { x, y, z } ) );
-		}
-		final DBSCANClusterer< DoublePoint > clusterer = new DBSCANClusterer<>( 3., 1 );
-		final List< Cluster< DoublePoint > > clusters = clusterer.cluster( junctionPixels );
+			for ( final Vertex vertex : vertices )
+			{
+				if ( vertex.getBranches().size() == 1 )
+					continue;
 
-		// Create one spot per cluster.
-		for ( final Cluster< DoublePoint > cluster : clusters )
-		{
-			final double minX = cluster.getPoints().stream()
-					.mapToDouble( p -> ( startx + p.getPoint()[ 0 ] ) * calibration[ 0 ] )
-					.min().getAsDouble();
-			final double maxX = cluster.getPoints().stream()
-					.mapToDouble( p -> ( startx + p.getPoint()[ 0 ] ) * calibration[ 0 ] )
-					.max().getAsDouble();
-			final double meanX = cluster.getPoints().stream()
-					.mapToDouble( p -> ( startx + p.getPoint()[ 0 ] ) * calibration[ 0 ] )
-					.average().getAsDouble();
+				final Spot spot = vertexToSpot( vertex, calibration, topLeft );
+				vertexMap.put( vertex, spot );
+				spots.add( spot );
+			}
 
-			final double minY = cluster.getPoints().stream()
-					.mapToDouble( p -> ( starty + p.getPoint()[ 1 ] ) * calibration[ 1 ] )
-					.min().getAsDouble();
-			final double maxY = cluster.getPoints().stream()
-					.mapToDouble( p -> ( starty + p.getPoint()[ 1 ] ) * calibration[ 1 ] )
-					.max().getAsDouble();
-			final double meanY = cluster.getPoints().stream()
-					.mapToDouble( p -> ( starty + p.getPoint()[ 1 ] ) * calibration[ 1 ] )
-					.average().getAsDouble();
+			/*
+			 * Find end points and link them to their junction.
+			 */
 
-			final double minZ = cluster.getPoints().stream()
-					.mapToDouble( p -> ( startz + p.getPoint()[ 2 ] ) * calibration[ 2 ] )
-					.min().getAsDouble();
-			final double maxZ = cluster.getPoints().stream()
-					.mapToDouble( p -> ( startz + p.getPoint()[ 2 ] ) * calibration[ 2 ] )
-					.max().getAsDouble();
-			final double meanZ = cluster.getPoints().stream()
-					.mapToDouble( p -> ( startz + p.getPoint()[ 2 ] ) * calibration[ 2 ] )
-					.average().getAsDouble();
+			for ( final Vertex vertex : vertices )
+			{
+				if ( vertex.getBranches().size() != 1 )
+					continue;
 
-			final double dx = maxX - minX;
-			final double dy = maxY - minY;
-			final double dz = maxZ - minZ;
-			final double radius = 0.5 * Math.sqrt( dx * dx + dy * dy + dz * dz );
-			final double quality = JUNCTION_POINTS_QUALITY_VALUE;
-			final Spot spot = new Spot( meanX, meanY, meanZ, radius, quality );
-			spots.add( spot );
+				final Spot spot = vertexToSpot( vertex, calibration, topLeft );
+				final Edge predecessor = vertex.getBranches().get( 0 );
+				if ( null == predecessor )
+					continue;
+
+				final Vertex oppositeVertex = predecessor.getOppositeVertex( vertex );
+				final Spot junctionSpot = vertexMap.get( oppositeVertex );
+				if ( null != junctionSpot )
+				{
+					spot.setName( "->" + junctionSpot.ID() );
+					spot.putFeature( JunctionIDAnalyzerFactory.FEATURE, Double.valueOf( junctionSpot.ID() ) );
+					final double branchLength = predecessor.getLength_ra();
+					spot.putFeature( BranchLengthAnalyzerFactory.FEATURE, Double.valueOf( branchLength ) );
+
+				}
+				else
+				{
+					spot.setName( "No junction" );
+				}
+				spots.add( spot );
+			}
+
 		}
 
 		final long end = System.currentTimeMillis();
 		this.processingTime = end - start;
 
 		return true;
+	}
+
+	private static final Spot vertexToSpot( final Vertex vertex, final double[] calibration, final long[] start )
+	{
+		final List< Point > points = vertex.getPoints();
+		final double xi = points.stream()
+				.mapToDouble( p -> p.x )
+				.average()
+				.getAsDouble();
+		final double yi = points.stream()
+				.mapToDouble( p -> p.y )
+				.average()
+				.getAsDouble();
+		final double zi = points.stream()
+				.mapToDouble( p -> p.z )
+				.average()
+				.getAsDouble();
+
+		final boolean isJunction = vertex.getBranches().size() > 1;
+
+		final double x = ( start[ 0 ] + xi ) * calibration[ 0 ];
+		final double y = ( start[ 1 ] + yi ) * calibration[ 1 ];
+		final double z = ( start[ 2 ] + zi ) * calibration[ 2 ];
+		final double radius = isJunction ? 1. : 0.5;
+		final double quality = isJunction ? JUNCTION_POINTS_QUALITY_VALUE : END_POINTS_QUALITY_VALUE;
+		final Spot spot = new Spot( x, y, z, radius, quality );
+		return spot;
 	}
 
 	@Override
