a7afcc2a3cb29894a2e4f56b25d80cad14c4190c
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-09-06 18:22:50 +0200

    Load demo image with IJ1 to avoid issues with virtual images.

--- src/test/java/fr/pasteur/iah/dendritedynamicstracker/TestDriveCommand.java
+++ src/test/java/fr/pasteur/iah/dendritedynamicstracker/TestDriveCommand.java
@@ -2,6 +2,7 @@ package fr.pasteur.iah.dendritedynamicstracker;
 
 import java.io.IOException;
 
+import ij.io.Opener;
 import net.imagej.ImageJ;
 
 public class TestDriveCommand
@@ -9,14 +10,10 @@ public class TestDriveCommand
 
 	public static void main( final String[] args ) throws IOException
 	{
-		final String traceImgName = "samples/Example3-dendrites_traced.tif";
-
 		final ImageJ ij = new ImageJ();
 		ij.launch( args );
-
-		final Object obj = ij.io().open( traceImgName );
-		ij.ui().show( obj );
-
+		final String traceImgName = "samples/Example3-dendrites_traced-1.tif";
+		new Opener().openImage( traceImgName ).show();
 		ij.command().run( DendriteDynamicsTrackerCommand.class, true );
 	}
 
