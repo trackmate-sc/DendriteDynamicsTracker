ab5acd62238b688008a012b14ff59e07079018b2
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-10-07 16:14:11 +0200

    Change the name of some constants.

--- src/main/java/fr/pasteur/iah/dendritedynamicstracker/trackmate/tracking/SkeletonEndPointTrackerFactory.java
+++ src/main/java/fr/pasteur/iah/dendritedynamicstracker/trackmate/tracking/SkeletonEndPointTrackerFactory.java
@@ -22,11 +22,11 @@ import fiji.plugin.trackmate.tracking.sparselap.SparseLAPTrackerFactory;
 @Plugin( type = SpotTrackerFactory.class )
 public class SkeletonEndPointTrackerFactory extends SparseLAPTrackerFactory
 {
-	public static final String THIS2_TRACKER_KEY = "SKELETON_END_POINT_TRACKER";
+	public static final String SKELETON_TRACKER_KEY = "SKELETON_END_POINT_TRACKER";
 
-	public static final String THIS2_NAME = "Skeleton end-point tracker";
+	public static final String SKELETON_TRACKER_NAME = "Skeleton end-point tracker";
 
-	public static final String THIS2_INFO_TEXT = "<html>"
+	public static final String SKELETON_TRACKER_INFO_TEXT = "<html>"
 			+ "This tracker tracks end-points of a skeleton."
 			+ "<p>"
 			+ "It privilieges matching end-points that connect to the same junction track. <br>"
@@ -39,19 +39,19 @@ public class SkeletonEndPointTrackerFactory extends SparseLAPTrackerFactory
 	@Override
 	public String getKey()
 	{
-		return THIS2_TRACKER_KEY;
+		return SKELETON_TRACKER_KEY;
 	}
 
 	@Override
 	public String getName()
 	{
-		return THIS2_NAME;
+		return SKELETON_TRACKER_NAME;
 	}
 
 	@Override
 	public String getInfoText()
 	{
-		return THIS2_INFO_TEXT;
+		return SKELETON_TRACKER_INFO_TEXT;
 	}
 
 	@Override
@@ -64,7 +64,7 @@ public class SkeletonEndPointTrackerFactory extends SparseLAPTrackerFactory
 	public ConfigurationPanel getTrackerConfigurationPanel( final Model model )
 	{
 		final String spaceUnits = model.getSpaceUnits();
-		return new SimpleLAPTrackerSettingsPanel( getName(), THIS2_INFO_TEXT, spaceUnits );
+		return new SimpleLAPTrackerSettingsPanel( getName(), SKELETON_TRACKER_INFO_TEXT, spaceUnits );
 	}
 
 
